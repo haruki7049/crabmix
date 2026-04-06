2520d5f188a3eec6e6d4033b36cab88853035649
haruki7049 <tontonkirikiri@gmail.com>
2026-04-07 00:44:23 +0900

    Update

--- crates/riffy_chan/src/lib.rs
+++ crates/riffy_chan/src/lib.rs
@@ -1,8 +1,16 @@
 //! # riffy_chan
 
+use thiserror::Error;
+
 #[derive(Debug, Default, PartialEq, Eq)]
 pub struct FourCC {
     inner: [u8; 4],
+}
+
+impl From<FourCC> for Vec<u8> {
+    fn from(value: FourCC) -> Self {
+        value.inner.to_vec()
+    }
 }
 
 impl From<[u8; 4]> for FourCC {
@@ -22,8 +30,50 @@ pub enum Chunk {
     Riff { four_cc: FourCC, chunks: Vec<Chunk> },
 }
 
+impl TryFrom<Vec<u8>> for Chunk {
+    type Error = ChunkTryFromError;
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        let riff: FourCC = FourCC::from([b'R', b'I', b'F', b'F']);
+        let list: FourCC = FourCC::from([b'L', b'I', b'S', b'T']);
+
+        let four_cc_raw = value
+            .first_chunk::<4>()
+            .ok_or(ChunkTryFromError::NoFourCC)?;
+        let four_cc: FourCC = FourCC::from(*four_cc_raw);
+        let rest: Vec<u8> = value.iter().skip(4).cloned().collect();
+
+        match four_cc {
+            r if r == riff => Self::create_riff_chunk(rest),
+            l if l == list => Self::create_list_chunk(rest),
+            _ => Self::create_chunk(four_cc, rest),
+        }
+    }
+}
+
+impl Chunk {
+    fn create_chunk(four_cc: FourCC, data: Vec<u8>) -> Result<Self, ChunkTryFromError> {
+        let v = Chunk::Chunk { four_cc, data };
+        Ok(v)
+    }
+
+    fn create_list_chunk(value: Vec<u8>) -> Result<Self, ChunkTryFromError> {
+        todo!()
+    }
+
+    fn create_riff_chunk(value: Vec<u8>) -> Result<Self, ChunkTryFromError> {
+        todo!()
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ChunkTryFromError {
+    #[error("There is not a FourCC data")]
+    NoFourCC,
+}
+
 #[cfg(test)]
-mod tests {
+mod four_cc_tests {
     use super::FourCC;
 
     #[test]
